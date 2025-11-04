ca149c3ba142af28d91b0e33bbde306b6098126f
Kat <katherinekelli.creates@gmail.com>
2025-11-04 12:00:19 -0600

    Initial Commit

---
+++ Three letter word/Three_letter_wordApp.swift
@@ -0,0 +1,17 @@
+//
+//  Three_letter_wordApp.swift
+//  Three letter word
+//
+//  Created by Katherine Pizzirusso on 11/4/25.
+//
+
+import SwiftUI
+
+@main
+struct Three_letter_wordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
